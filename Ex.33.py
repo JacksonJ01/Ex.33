9f00dff807fce93e5f7215ab5ecf6c32d832c8b5
JacksonJ01 <jacksonj@hartwick.edu>
2019-10-09 16:44:20 -0400

    doing this exercise helped me realize my mistake in the last exercise
    -Jared Jackson

--- Ex.33.py
+++ Ex.33.py
@@ -1,1 +1,20 @@
+# Jared Jackson
+# 10/09/19
+# we will be learning about While Loops
 
+i = 0
+
+numbers = []
+
+while i < 6:
+    print(f"Atb the top i is {i}")
+    numbers.append(i)
+
+    i = i + 1
+    print("Numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
